9947babbb0eedb6250ebd164cccc5296dc80da8c
Anu Rajput <Anu Rajput>
2017-12-02 12:08:42 +0530

    updated host

--- app.py
+++ app.py
@@ -320,4 +320,4 @@ def comparison():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='127.0.0.1', port=port, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
